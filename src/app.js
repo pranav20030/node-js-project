23888388b7ea8e0f95af2e244001b261fed972cb
Pranav Gupta <pranavg9876@gmail.com>
2025-11-14 12:23:45 +0530

    all config setup

--- src/app.js
+++ src/app.js
@@ -14,5 +14,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRouter)
+
+// http://localhost:8000/api/vi/users/register
+
+
 
 export {app}
